baf852ca0bbf5b98c86274522b86a47f0929c18f
nefrttUnknown <39979965+nefrttPrabhu@users.noreply.github.com>
2024-10-22 14:02:39 +0530

    Delete Amazon_clone-main/Amazon_clone-main directory

--- Amazon_clone-main/Amazon_clone-main/src/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  // axios is used to get or post request its better than fetchAPI
-  baseURL: "http://localhost:5001/clone-f3161/us-central1/api", // The API (cloud function) URL
-});
-
-export default instance;
